d28a8779d9ee71f4e7434ad81df6290b4f1bd559
Poncho <Johanpoldaru@gmail.com>
2021-04-04 13:38:49 +0300

    Update guessing_game.py

--- scripts/guessing_game.py
+++ scripts/guessing_game.py
@@ -1,40 +1,31 @@
 import socket
 
-hostname = 'challenges.ctfd.io'
-port = 30005
-
 # Initialize socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((hostname, port))
+sock.connect(('challenges.ctfd.io', 30005))
 
 while True:
-    # Set initial chunk size, make sure to keep collecting until server stops sending
-    chunk = sock.recv(8192)
-    while not chunk.endswith(b'\n'):
-        chunk += sock.recv(1000)
+    # Receive up to 8192 bytes, decode. If issues, += to chunk with while not endswith \n
+    chunk = sock.recv(8192).decode('utf-8')
 
     # Look for end condition (predicted flag)
-    if 'UDCTF' in chunk.decode('utf-8'):
-        print(chunk.decode('utf-8'))
+    if 'UDCTF' in chunk or 'Sorry' in chunk:
+        print(chunk)
         break
 
     # Parse output
-    lines = [chunk.decode('ascii').split('\n')]
-    flat_lines = [item for sublist in lines for item in sublist]
+    lines = chunk.split('\n')
+    all_bytes = lines[2].split('\\')[1:]
+    low_bytes, high_bytes = [], []
 
     # Logic
-    bytes = flat_lines[2].split('\\')[1:]
-    low_bytes = []
-    high_bytes = []
-
-    for i in bytes:
+    for i in all_bytes:
         if i in ['x00', 'x01', 'x02', 'x03', 'x04', 'x05']:
             low_bytes.append(i)
         if i in ['xff', 'xfe', 'xfd', 'xfc', 'xfb', 'xfa']:
             high_bytes.append(i)
 
-    low = len(low_bytes)
-    high = len(high_bytes)
+    low, high = len(low_bytes), len(high_bytes)
 
     if low == 7 and high == 1 or high == 7 and low == 1:
         result = 'xor'
@@ -46,10 +37,9 @@ while True:
         result = 'xor'
 
     # Show script progress
-    for i in flat_lines:
-        print(i)
+    [print(i) for i in lines]
     print(f'Low bytes: {low}\nHigh bytes: {high}\nPrediction: {result}\n')
 
     # Encode and transfer
-    data = str(result).encode('utf-8') + b'\n'
+    data = result.encode('utf-8') + b'\n'
     sock.send(data)
